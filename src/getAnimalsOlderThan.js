0126a0653582171139db53d6d316009d877db89e
cafe51 <cafecafe51@hotmail.com>
2022-03-29 02:59:32 -0400

    questao 3 resolvida

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+function achaAnimais(animal) {
+  const especie = species.find((element, index) => animal === species[index].name);
+  return especie.residents;
+}
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animais = achaAnimais(animal);
+  return animais
+    .map((element) => element.age)
+    .every((element) => element > age);
 }
+
+
+
+
+
+
+// function achaAnimais(animal) {
+//   return species.reduce((acc, result) => {
+//     if (result.name === animal) {
+//       return acc;
+//     }
+//     return acc;
+//   }).residents;
+// }
+
+console.log(getAnimalsOlderThan('penguins', 10));
+// console.log(achaAnimais('lions'));
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+function getEmployeeByName(employeeName = 'vazio') {
   // seu código aqui
+  if (employeeName === 'vazio') return {};
+  function funcaoInterna(element) {
+    return employeeName === element.firstName
+    || employeeName === element.lastName;
+  }
+  return employees.find(funcaoInterna);
 }
 
 module.exports = getEmployeeByName;
